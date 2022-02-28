500932d695258034a9c6e94492d5900864b083af
VictorR15 <victorramirez01@aragon.unam.mx>
2022-02-28 00:38:52 -0600

    Proyecto conceptos basicos

---
+++ Clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -0,0 +1,13 @@
+
+package conceptosbasicos;
+
+
+public class ConceptosBasicos {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola de nuevo");
+    }
+    
+}
